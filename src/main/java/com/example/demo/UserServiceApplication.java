1115c8366d49485b338dc32f02072560410b01ff
sayangit03 <sayman.eye@gmail.com>
2020-09-12 11:18:48 +0530

    test checkin

--- src/main/java/com/example/demo/UserServiceApplication.java
+++ src/main/java/com/example/demo/UserServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
