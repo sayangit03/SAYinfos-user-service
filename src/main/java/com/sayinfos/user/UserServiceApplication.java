0eda030ffc3a97480e693c440f892ac81dea50d2
sayangit03 <sayman.eye@gmail.com>
2021-05-27 20:24:47 +0530

    change from war to jar

--- src/main/java/com/sayinfos/user/UserServiceApplication.java
+++ src/main/java/com/sayinfos/user/UserServiceApplication.java
@@ -5,8 +5,6 @@ package com.sayinfos.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,16 +15,18 @@ import brave.sampler.Sampler;
 @SpringBootApplication
 //@EnableDiscoveryClient
 @ComponentScan(basePackages = { "com.sayinfos.user.bean", "com.sayinfos.user.repository", "com.sayinfos.user.controller" })
-public class UserServiceApplication extends SpringBootServletInitializer {
+public class UserServiceApplication /* extends SpringBootServletInitializer */ {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(UserServiceApplication.class);
-	}
+
+	/*
+	 * @Override protected SpringApplicationBuilder
+	 * configure(SpringApplicationBuilder builder) { return
+	 * builder.sources(UserServiceApplication.class); }
+	 */
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
